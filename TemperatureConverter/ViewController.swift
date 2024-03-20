751395a23752e28790bae5a333d389472b59fda7
Maida <98819095+maidads@users.noreply.github.com>
2024-03-20 15:47:32 +0100

    Fixed so it converts right after last implementation

--- TemperatureConverter/ViewController.swift
+++ TemperatureConverter/ViewController.swift
@@ -24,6 +24,7 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
         
         let defaultRow = tempValues.count / 2
         celciusPickerView.selectRow(defaultRow, inComponent: 0, animated: false)
+        pickerView(celciusPickerView, didSelectRow: defaultRow, inComponent: 0)
         
     }
     
