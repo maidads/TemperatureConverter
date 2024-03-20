42604d1ca51908be0df0d2901b0b61ddfe753a84
Maida <98819095+maidads@users.noreply.github.com>
2024-03-20 15:10:23 +0100

    Number of components in our pickerView function done - > return 1

--- TemperatureConverter/ViewController.swift
+++ TemperatureConverter/ViewController.swift
@@ -7,13 +7,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIPickerViewDataSource {
+    
     @IBOutlet weak var temperatureLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        <#code#>
     }
 
 
